b48a37d7b5467c634b0ecee40e26c97dd6e4fadc
Denton Woods <denton.woods@gmail.com>
2023-01-02 12:16:09 -0600

    Miscellaneous updates
    - Added generating an export header
    - Updated download URL for HydrOffice Sound Speed in examples
    - Switched to header-only fmt library
    - Updated file header comments with new year

--- src/Readers/Asvp.cpp
+++ src/Readers/Asvp.cpp
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2021 Denton Woods
+ * Copyright (c) 2023 Denton Woods
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
